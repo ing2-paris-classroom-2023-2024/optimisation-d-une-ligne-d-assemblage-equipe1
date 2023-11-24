ff9f625e190d2f1cec8926c31c0ad453365d6f47
HugoChrt21 <131973133+HugoChrt21@users.noreply.github.com>
2023-11-24 19:38:45 +0100

    ssp opérations sans précedences

--- PartieHugo.c
+++ PartieHugo.c
@@ -1,1 +1,18 @@
+void trouverRacine(struct Operation listeOp[100],int nmbOp) // permet de marquer les operations qui n'ont pas de arc entrant ( pas de precedents)
+{
+    for(int i = 0 ; i< nmbOp ; i++)
+    {
+        for(int k = 0 ; k< nmbOp ; k++)
+        {
+            for(int l = 0 ; l < listeOp[k].nmbS ; l++)
+            {
+                if(getOpById(listeOp[k].suivant[l],listeOp,nmbOp).id == listeOp[i].id)
+                {
+                    listeOp[i].racine = 1;
+                }
+            }
+
+        }
+    }
+}
 
