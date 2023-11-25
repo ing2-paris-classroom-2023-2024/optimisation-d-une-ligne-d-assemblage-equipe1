3a782c5db4605c488be00a248cab190432e9f081
HugoChrt21 <131973133+HugoChrt21@users.noreply.github.com>
2023-11-25 12:27:13 +0100

    DFS et commentaires

--- PartieHugo.c
+++ PartieHugo.c
@@ -4,15 +4,54 @@ void trouverRacine(struct Operation listeOp[100],int nmbOp) // permet de marquer
     {
         for(int k = 0 ; k< nmbOp ; k++)
         {
-            for(int l = 0 ; l < listeOp[k].nmbS ; l++)
+            for(int l = 0 ; l < listeOp[k].nmbS ; l++) // Parcours de tous les successeurs de l'opération actuelle
             {
-                if(getOpById(listeOp[k].suivant[l],listeOp,nmbOp).id == listeOp[i].id)
+                if(getOpById(listeOp[k].suivant[l],listeOp,nmbOp).id == listeOp[i].id) // Vérifie si l'opération actuelle est un successeur de l'opération en cours
                 {
-                    listeOp[i].racine = 1;
+                    listeOp[i].racine = 1; // Si l'opération actuelle a un successeur alors on met racine à 1
                 }
             }
 
         }
     }
+}
+
+
+int DFS(struct Operation listeOp[100],int nmbOp,struct Operation depart, struct Operation arrive)
+{
+    struct Operation pile[15];
+    int taillePile = 0;
+    pile[0] = depart;
+
+
+    do
+    {
+        int act = taillePile;
+        //printf("\non analyse les suivant de %d , %d suivants\n",pile[act].id,pile[act].nmbS);
+        //system("pause");
+
+        for(int i = 0 ; i < pile[act].nmbS ; i++) // Parcourir les successeurs du nœud actuel
+        {
+            //printf("\non ajoute %d\n",pile[act].suivant[i]);
+            //system("pause");
+            if(pile[act].suivant[i] == arrive.id)
+            {
+                return 1;
+            }
+            struct Operation suivant = getOpById(pile[act].suivant[i],listeOp,nmbOp); // Récupération du nœud suivant à partir de son identifiant
+            
+            if(suivant.placed == 1) //nœud suivant n'a pas déjà été visité, l'ajouter à la pile
+            {
+                pile[taillePile] = suivant;
+                taillePile++;
+            }
+        }
+        taillePile--;
+
+
+    }
+    while(taillePile > -1);
+
+    return 0;
 }
 
