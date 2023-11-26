afa68f7ff480771046a5ee4353dac9e673607d40
thomassbgne <132274990+thomassbgne@users.noreply.github.com>
2023-11-26 22:49:17 +0100

    Intégration du ssprog comparateur de classement

--- PartieThomas.c
+++ PartieThomas.c
@@ -1,1 +1,6 @@
 Partie de code de Thomas
+
+int comparerByClassement(const void *a, const void *b)   // fonction pour classer la liste d'peration en fonction de l'ordre dans le tri topologique
+{
+    return ((struct Operation *)b)->classement - ((struct Operation *)a)->classement;
+}
